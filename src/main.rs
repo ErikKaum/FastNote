a60fed24269287ba7c6c58fb5d07debd91c7d07a
ErikKaum <erik.kaum@gmail.com>
2022-10-24 22:20:27 +0300

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
 use clap::{Command, arg, ArgMatches };
+use serde::{Serialize,Deserialize};
 use std::fs::File;
 use std::fs::read_dir;
 use std::io::Error;
 
-fn init() {
-    println!("called init function");
+#[derive(Debug, Serialize, Deserialize)]
+enum Editor {
+    Vim,
+    Nano
 }
 
-fn add(name :&ArgMatches) -> Result<&String, Error> {
+#[derive(Debug, Serialize, Deserialize)]
+struct Conf {
+    path: String,
+    editor: Editor,
+}
+
+
+fn init() -> Result<(), confy::ConfyError> {
+
+    let test_conf = Conf {
+        path: String::from("./data/"),
+        editor: Editor::Vim,
+    };
+    confy::store("fastnote", "fastnote-conf", test_conf)?;
+    println!("=> FastNote ready to use!");
+    println!("=> To create your first note, type: fastnote new [NOTE_NAME]");
+    Ok(())
+}
+
+fn add(name :&ArgMatches) -> Result<(), Error> {
+    
+    let base = String::from("./data/");
     let note_name = name.get_one::<String>("NOTE_NAME").unwrap();
-    File::create(note_name)?;
-    Ok(note_name)
+    
+    let full_path = format!("{}{}", base, note_name);
+    
+    File::create(full_path)?;
+    Ok(())
 }
 
 fn ls() {
-    let files = read_dir("./").unwrap();
+    let files = read_dir("./data/").unwrap();
     for file in files {
         let path = file.unwrap().path();
         let name = path.to_str();
         match name {
             None => {},
-            Some(f) => println!("{}", f.replace("./", ""))
+            // Some(f) => println!("{}", f.replace("./", ""))
+            Some(f) => println!("{}", f)
         }
     }
 }
@@ -55,13 +83,13 @@ fn main() {
         .get_matches();
 
         match matches.subcommand() {
-            Some(("init", _sub_m)) => init(),
+            Some(("init", _sub_m)) => init().unwrap(),
             Some(("ls", _sub_m)) => ls(),
             Some(("del", sub_m)) => del(&sub_m),
             Some(("new", sub_m)) => {
                 let res = add(&sub_m);
                 match res {
-                    Ok(res) => println!("{res}"),
+                    Ok(_) => {},
                     Err(e) => panic!("{}", e)
                 }
             },
